6ff1c9657807c43d66049f8ffb13df76de7fe8ff
Wouter de Vries <wdevries@leukeleu.nl>
2025-04-23 15:18:01 +0200

    Use password for menu icon

--- hidp_wagtail/wagtail_hooks.py
+++ hidp_wagtail/wagtail_hooks.py
@@ -8,6 +8,6 @@ def register_custom_menu_item():
     return MenuItem(
         "Account security",
         reverse("hidp_account_management:manage_account"),
-        icon_name="folder-inverse",
+        icon_name="password",
         order=1000,
     )
